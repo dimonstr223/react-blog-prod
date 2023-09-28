57fac3dbfa1ec116b84df0da370850fad22e97a2
dimonstr223 <dimstukmikolu@gmail.com>
2023-09-28 09:02:23 +0300

    2.6 Webpack hot module replacement

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,10 +1,11 @@
 import {BuildOptions} from "./types/config";
 import HtmlWebpackPlugin from 'html-webpack-plugin'
-import {DefinePlugin, ProgressPlugin, WebpackPluginInstance} from "webpack";
+import {DefinePlugin, HotModuleReplacementPlugin, ProgressPlugin, WebpackPluginInstance} from "webpack";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ReactRefreshWebpackPlugin  from "@pmmmwh/react-refresh-webpack-plugin";
 
 export const buildPlugins = ({ paths, isDev }: BuildOptions): WebpackPluginInstance[] => {
-  return [
+  const plugins = [
     new HtmlWebpackPlugin({ template: paths.html }),
     new ProgressPlugin(),
     new MiniCssExtractPlugin({
@@ -13,6 +14,13 @@ export const buildPlugins = ({ paths, isDev }: BuildOptions): WebpackPluginInsta
     }),
     new DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev)
-    })
+    }),
   ]
+
+  if (isDev) {
+    plugins.push(new ReactRefreshWebpackPlugin())
+    plugins.push(new HotModuleReplacementPlugin())
+  }
+
+  return plugins
 }
