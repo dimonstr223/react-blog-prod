94dcdb2f460dcb866cb94020be1513d028b77b43
dimonstr223 <dimstukmikolu@gmail.com>
2023-10-18 11:31:25 +0300

    3.1 ErrorBoundary

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import { render } from 'react-dom'
-import App from 'app/App'
 import { BrowserRouter } from 'react-router-dom'
-import { ThemeProvider } from 'app/providers/ThemeProvider'
 import 'shared/config/i18n/i18n'
+
+import { ThemeProvider } from 'app/providers/ThemeProvider'
+import { ErrorBoundary } from 'app/providers/ErrorBoundary'
+import App from 'app/App'
 
 render(
   <BrowserRouter>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <ErrorBoundary>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </ErrorBoundary>
   </BrowserRouter>,
   document.getElementById('root')
 )
---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react'
+import { useTranslation } from 'react-i18next'
+
+import { classNames } from 'shared/lib/classNames/classNames'
+import { Button } from 'shared/ui/Button/Button'
+
+import cls from './PageError.module.scss'
+
+interface PageErrorProps {
+  className?: string
+}
+
+export const PageError: FC<PageErrorProps> = ({ className }) => {
+  const { t } = useTranslation()
+  const reloadPage = () => location.reload()
+
+  return (
+    <div className={classNames(cls.PageError, [className])}>
+      <div>{t('Произошла непредвиденная ошибка')}</div>
+      <Button onClick={reloadPage}>{t('Обновить страницу')}</Button>
+    </div>
+  )
+}
