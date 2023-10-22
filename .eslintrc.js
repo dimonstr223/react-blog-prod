40365fa66b524b29d3523ed216b5915811b392d5
dimonstr223 <dimstukmikolu@gmail.com>
2023-10-22 21:24:40 +0300

    3.4 add storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,50 +2,45 @@ module.exports = {
   'env': {
     'browser': true,
     'es2021': true,
-    'jest': true,
+    'jest': true
   },
-  'extends': [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:i18next/recommended'
-  ],
-  'overrides': [
-    {
-      'env': {
-        'node': true,
-      },
-      'files': [
-        '.eslintrc.{js,cjs}',
-        '**/src/**/*.test.{ts,tsx}'
-      ],
-      rules: { 'i18next/no-literal-string': 'off' },
-      'parserOptions': {
-        'sourceType': 'script'
-      }
+  'extends': ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended', 'plugin:i18next/recommended'],
+  'overrides': [{
+    'env': {
+      'node': true
+    },
+    'files': ['.eslintrc.{js,cjs}', '**/src/**/*.test.{ts,tsx}'],
+    rules: {
+      'i18next/no-literal-string': 'off'
+    },
+    'parserOptions': {
+      'sourceType': 'script'
     }
-  ],
+  }],
   'parser': '@typescript-eslint/parser',
   'parserOptions': {
     'ecmaVersion': 'latest',
     'sourceType': 'module'
   },
-  'plugins': [
-    'react',
-    '@typescript-eslint',
-    'i18next'
-  ],
+  'plugins': ['react', '@typescript-eslint', 'i18next'],
   'rules': {
-    'indent': [ 2, 2 ],
-    'linebreak-style': [ 'error', 'windows' ],
-    'quotes': [ 'error', 'single' ],
-    'semi': [ 'error', 'never' ],
-    '@typescript-eslint/no-unused-vars': ['warn', { 'argsIgnorePattern': '^_' }],
+    'indent': [2, 2],
+    'linebreak-style': ['error', 'windows'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
+    '@typescript-eslint/no-unused-vars': ['warn', {
+      'argsIgnorePattern': '^_'
+    }],
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
     'react/no-deprecated': 'off',
-    'i18next/no-literal-string': ['warn', { markupOnly: true, ignoreAttribute: ['data-testid'] }],
+    'i18next/no-literal-string': ['warn', {
+      markupOnly: true,
+      ignoreAttribute: ['data-testid', 'to']
+    }],
     'object-curly-spacing': ['error', 'always']
   },
-  'globals': { __IS_DEV__: true }
+  'globals': {
+    __IS_DEV__: true
+  }
 }
--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,6 @@
-import { BuildOptions } from './types/config'
 import { RuleSetRule } from 'webpack'
-import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import { BuildOptions } from './types/config'
+import { buildCssLoader } from './loaders/buildCssLoader'
 
 export const buildLoaders = ({ isDev }: BuildOptions): RuleSetRule[] => {
   const tsLoader = {
@@ -20,28 +20,7 @@ export const buildLoaders = ({ isDev }: BuildOptions): RuleSetRule[] => {
     }
   }
 
-  const cssLoader = {
-    test: /\.s[ac]ss$/i,
-    use: [
-      // Creates `style` nodes from JS strings
-      // "style-loader", //заменил на MiniCssExtractPlugin (создает файл css в build)
-      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-      // Translates CSS into CommonJS
-      {
-        loader: 'css-loader',
-        options: {
-          modules: {
-            // модульные классы только в файлах scss с '.module.'
-            auto: /\.module\./,
-            // наименование классов при dev / prod сборке
-            localIdentName: isDev ? '[path][name]__[local]--[hash:base64:5]' : '[hash:base64:8]'
-          }
-        }
-      },
-      // Compiles Sass to CSS
-      'sass-loader'
-    ]
-  }
+  const cssLoader = buildCssLoader(isDev)
 
   const svgLoader = {
     test: /\.svg$/,
